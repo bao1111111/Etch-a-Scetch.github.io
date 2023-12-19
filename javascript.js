3d853913f486e101a60b1b230e71fd968970f967
Bao <143947499+Bao1111111@users.noreply.github.com>
2023-12-19 16:29:43 +0100

    making loop for Grids

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
-let body = document.querySelector("body")
+
+let body = document.getElementsByClassName("container")
 let div = document.createElement("div")
 body.appendChild(div)
+
+// Creating Grid 16*16
+function createGrid(){
+    for(let i = 0; i < 16; i++){
+        
+    }
+}
