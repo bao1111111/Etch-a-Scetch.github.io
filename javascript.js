8df79c0427110f7db536374bbd611d7764779ab0
Bao <143947499+Bao1111111@users.noreply.github.com>
2023-12-21 14:48:06 +0100

    move javascript to external javascript file

--- javascript.js
+++ javascript.js
@@ -1,11 +1,22 @@
+function createGrid() {
+    const gridSize = parseInt(document.getElementById("gridSize").value);
+    const etchASketch = document.getElementById("etchASketch");
 
-let body = document.getElementsByClassName("container")
-let div = document.createElement("div")
-body.appendChild(div)
+    // Clear existing grid
+    etchASketch.innerHTML = "";
 
-// Creating Grid 16*16
-function createGrid(){
-    for(let i = 0; i < 16; i++){
-        
+    // Create the grid
+    for (let i = 0; i < gridSize; i++) {
+      const row = etchASketch.insertRow(i);
+      for (let j = 0; j < gridSize; j++) {
+        const cell = row.insertCell(j);
+
+        // Add event listener for drawing on mouseover
+        cell.addEventListener("mouseover", function() {
+          cell.style.backgroundColor = "green";
+        });
+      }
     }
-}
+  }
+// Initial grid creation
+createGrid();
